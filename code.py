1bf09d8f801871d56bfa1c679260bb5374a2afa4
Sandeep Reddy Dodda <85955943+sandeepreddydodda@users.noreply.github.com>
2021-11-23 23:28:40 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+dict = {'area':[2600, 3000, 3200, 3600, 4000], 'price':[550000, 565000, 610000, 680000, 725000]}
+df = pd.DataFrame(dict)
+df
+
+%matplotlib inline
+plt.scatter(df.area, df.price, color='red', marker='+')
+
+reg = linear_model.LinearRegression()
+reg.fit(df[['area']], df.price)
+
+reg.predict([[3300]])
+reg.coef_
+reg.intercept_
